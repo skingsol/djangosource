b1e88842edb01ebc7f79645e082005610d967ab6
skingsol <skingsol@naver.com>
2023-07-27 18:17:32 +0900

    당고 게시판정리

---
+++ board/static/js/list.js
@@ -0,0 +1,15 @@
+// 하단의 페이지 나누기 영역 클릭 시
+// href 값 가져오기
+
+document.querySelector(".pagination").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let href = e.target.getAttribute("href");
+  // console.log(href);
+
+  // 페이지 값이 있다면 페이지값을 업데이트하고 폼 제출
+  if (href) {
+    document.querySelector("#page").value = href;
+    document.querySelector('form[action=""]').submit();
+  }
+});
